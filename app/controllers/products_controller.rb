2e53aead5dfb038419d92cf9bfe73dd41ae5ddd0
Fh Ndiritu <fhndiritu@gmail.com>
2023-12-11 18:30:44 +0300

    add product images

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -65,6 +65,6 @@ class ProductsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def product_params
-      params.require(:product).permit(:title, :user_id)
+      params.require(:product).permit(:title, :user_id, images: []).merge(user: current_user)
     end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :user
   has_many :conversations, dependent: :destroy
+  has_many_attached :images do |attachable|
+    attachable.variant :thumb, resize_to_limit: [100, 100]
+  end
 end
