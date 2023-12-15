d913cae3ca6da2459167486a9e5d942069bd5c63
Fh Ndiritu <fhndiritu@gmail.com>
2023-12-16 02:38:14 +0300

    get conversations on type

--- app/javascript/controllers/conversations_controller.js
+++ app/javascript/controllers/conversations_controller.js
@@ -2,9 +2,15 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="conversations"
 export default class extends Controller {
-  static targets = ["chat"]
+  static targets = ["chat", "query", "submit"]
 
   connect() {
     this.chatTarget.click()
+  }
+
+  search() {
+    if (this.queryTarget.value.length > 3 || this.queryTarget.value.length == 0) {
+      this.submitTarget.click()
+    }
   }
 }
