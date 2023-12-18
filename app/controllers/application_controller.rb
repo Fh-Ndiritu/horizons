cb7174836adea1f6265915fb0686e5720388fad8
Fh Ndiritu <fhndiritu@gmail.com>
2023-12-18 17:43:43 +0300

    detect mobile devices

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!
+    before_action :authenticate_user!, :check_mobile
+
+    private 
+
+    def check_mobile 
+      @is_mobile ||= /iphone|android|blackberry|mobile/.match?(request.user_agent.to_s.downcase)
+  end
+
 end
