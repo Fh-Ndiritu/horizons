a9c5a800fafde55dcd96ae8279599f0ccfdc28c7
Fh Ndiritu <fhndiritu@gmail.com>
2023-12-22 14:43:28 +0300

    uploading and displaying images

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,9 +5,29 @@ import "controllers"
 import "trix"
 import "@rails/actiontext"
 
+// import Trix from "trix"
 document.addEventListener('trix-initialize', function(event) {
    
-  
+//   alert("Nice")
 
 });
+
+document.addEventListener('trix-attachment-add', function(event) {
+    var attachment = event.attachment;
+
+  if (attachment.file) {
+    var file = attachment.file;
+    var maxSizeInBytes = 6 * 1024 * 1024; // 10 MB
+    var allowedTypes = ['image/jpeg', 'image/png', 'image/gif'];
+
+    if (file.size > maxSizeInBytes || !allowedTypes.includes(file.type)) {
+      alert('Invalid file. Please choose a smaller image file of type JPEG, PNG, or GIF.');
+      event.preventDefault();
+    }
+  }
+
+});
+
+var element = document.querySelector("trix-editor")
+element.editor  // is a Trix.Editor instance
 
--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,16 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :conversation
   has_rich_text :content
+  has_many_attached :files
   default_scope { order(created_at: :asc)}
-
   ThinkingSphinx::Callbacks.append(self, :behaviours =>[:real_time])
+include ActionText::Attachable
+  # app/views/users/_mention.html.erb 
+  # rendered in final rendering
+  def to_attachment_partial_path
+    "messages/attachments"
+  end
+
+
+
 end
