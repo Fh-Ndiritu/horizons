a5f79600bb19d5af4f54cc7fecba62648c8a9ad8
Fh Ndiritu <fhndiritu@gmail.com>
2023-12-22 15:47:48 +0300

    how to broadcast to websockets

--- app/models/message.rb
+++ app/models/message.rb
@@ -14,4 +14,18 @@ include ActionText::Attachable
 
 
 
+    after_create_commit -> { broadcast_append_to "chat_messages", partial: "messages/message", locals: { message: self, current_user: self.user } , target: "chat_messages"}
+
+  # after_update_commit -> { 
+  #   broadcast_replace_to "chat_messages", 
+  #   partial: "messages/message",
+  #   locals: {  message: self , current_user: self.user},
+  #   target: "chat_messages" 
+  # }
+  # after_destroy_commit -> { broadcast_remove_to "quotes" }
+
+
+
+
+
 end
